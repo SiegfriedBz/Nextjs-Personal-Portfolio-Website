7dc49659d8aaa0d555c488fb278633250387349a
siegfriedbz <siegfried.bozza@yahoo.com>
2024-06-19 13:53:29 +0800

    fix sentry GlobalError

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,19 +1,19 @@
-"use client";
+'use client'
 
-import * as Sentry from "@sentry/nextjs";
-import Error from "next/error";
-import { useEffect } from "react";
+import * as Sentry from '@sentry/nextjs'
+import Error from 'next/error'
+import { useEffect } from 'react'
 
-export default function GlobalError({ error }) {
+export default function GlobalError({ error }: { error: Error }) {
   useEffect(() => {
-    Sentry.captureException(error);
-  }, [error]);
+    Sentry.captureException(error)
+  }, [error])
 
   return (
     <html>
       <body>
-        <Error />
+        <Error statusCode={500} />
       </body>
     </html>
-  );
+  )
 }
