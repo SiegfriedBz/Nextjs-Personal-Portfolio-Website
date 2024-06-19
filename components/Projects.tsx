abae50ebb8ca4ae3fd725249ffe5bc9adc67e8f7
siegfriedbz <siegfried.bozza@yahoo.com>
2024-06-19 17:09:56 +0800

    update project cards

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -8,14 +8,14 @@ const Projects = () => {
       <h2 className='heading'>
         A selection of <span className='text-purple'> projects</span>
       </h2>
-      <div className='flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8'>
+      <div className='flex flex-wrap items-center justify-center p-4 gap-8 sm:gap-24'>
         {PROJECTS.map((project) => {
           return (
             <div
               key={project.id}
               className='flex items-center justify-center
-                h-[32rem] 
-                sm:h-[41rem]
+                h-[28rem] 
+                sm:min-h-[34rem]
                 lg:min-h-[32.5rem]
                 w-[80vw]
                 sm:w-[570px]
